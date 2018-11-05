d57a735775f6c9883ef2dee948f3c701da13ba69
Sabbir_Pulok <sabbir.pulak@gmail.com>
2018-11-05 15:52:36 -0800

    bugs fixed

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -6,7 +6,16 @@
             <h1>{{ $post->title }}</h1>
             <p>{{ $post->body }}</p>
             <hr>
-            <p>Posted In: {{ $post->category->name }}</p>
+            <p><strong>Posted In:</strong> {{ $post->category->name }}</p>
+            <hr>
+            <div class="tags">
+                <strong>Tags: </strong>
+                @foreach ($post->tags as $tag)
+                    <a href="#" class="btn btn-sm btn-info">{{ $tag->name }}</a>
+                    {{--<div class="d-inline p-2 bg-info text-white">{{ $tag->name }}</div>--}}
+                @endforeach
+            </div>
+
         </div>
     </div>
 
